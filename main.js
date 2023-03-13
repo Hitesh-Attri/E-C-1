3732d0a37ced1138c584b83f55230f5d185aaa4e
HiteshAttri <hiteshattri71@gmail.com>
2023-03-13 13:01:07 +0530

    npm i, i multer, added img upload and show img

--- main.js
+++ main.js
@@ -2,11 +2,17 @@ const express = require('express')
 const app = express();
 const fs = require('fs')
 const session = require('express-session');
+const multer = require('multer');
 app.set('view engine','ejs');
 const port = 5000;
 
 // for checking the auth.. if logged in
 var checkAuth = require('./middlewares/checkAuth');
+const { json } = require('express');
+
+const upload = multer ( { 
+    dest:'uploads'
+})
 
 app.use(session({
     secret:'keyboard cat',
@@ -17,6 +23,7 @@ app.use(session({
 app.use(express.json()); 
 app.use(express.urlencoded({ extended:true }));
 app.use(express.static('public'));
+app.use(express.static('uploads'));
 
 app.route('/').get((req,res)=>{
     if(!req.session.is_logged_in){
@@ -26,8 +33,27 @@ app.route('/').get((req,res)=>{
     }
 })
 
-app.route('/home').get((req,res)=>{
-    if(req.session.is_logged_in){
+// app.route('/home').get((req,res)=>{
+//     if(req.session.is_logged_in){
+//         // read product json and send to home
+//         let products;
+//         fs.readFile(__dirname+"/products.json",'utf-8', (err,data)=>{
+//             if(data.length === 0) products = [];
+//             else{
+//                 products = JSON.parse(data);
+//             }
+//             // console.log(products, typeof products);
+//             res.render('home', {username : req.session.username, loggedIn: req.session.is_logged_in, products: products})
+//         })
+//     }else{
+//         console.log("/home->else");
+//         // console.log(req.session.is_logged_in);
+//         res.redirect('/login');
+//     }
+// })
+
+app.route('/home').get( checkAuth, (req,res)=>{
+    // if(req.session.is_logged_in){
         // read product json and send to home
         let products;
         fs.readFile(__dirname+"/products.json",'utf-8', (err,data)=>{
@@ -38,11 +64,11 @@ app.route('/home').get((req,res)=>{
             // console.log(products, typeof products);
             res.render('home', {username : req.session.username, loggedIn: req.session.is_logged_in, products: products})
         })
-    }else{
-        console.log("/home->else");
+    // }else{
+        // console.log("/home->else");
         // console.log(req.session.is_logged_in);
-        res.redirect('/login');
-    }
+        // res.redirect('/login');
+    // }
 })
 
 app.route('/login').get((req,res)=>{
@@ -129,6 +155,26 @@ app.get('/getProducts',(req,res)=>{
         }
         res.json(theFile);
     });
+})
+
+app.post('/uploadProduct',upload.single('productImage'), (req,res)=>{
+    console.log("product img here");
+    let obj = {
+        productName:"Product Name",
+        fileName : req.file.filename
+    }
+    let theProducts;
+    fs.readFile(__dirname +"/products.json",'utf-8',(err,data)=>{
+        if(data.length === 0) theProducts = [];
+        else{
+            theProducts = JSON.parse(data);
+        }
+        theProducts.push(obj);
+        fs.writeFile(__dirname+'/products.json', JSON.stringify(theProducts),(err)=>{
+            console.log("products.json updated")
+        })
+    })
+    res.send(req.file);
 })
 
 app.get('/logout',(req,res)=>{
--- public/script/loadMoreScript.js
+++ public/script/loadMoreScript.js
@@ -23,9 +23,46 @@ rqst.addEventListener('load',()=>{
             console.log('loop');
             for(let i = curr; i < limit; i++){
                 console.log(i);
+
                 let li = document.createElement('li');
-                li.innerText = products[i];
+
+                let div = document.createElement('div');
+
+                let img = document.createElement('img');
+
+                img.setAttribute('src', products[i].fileName );
+
+                // for pruduct name
+                let p = document.createElement('p');
+                p.innerText = products[i].productName;
+                
+                let VDbtn = document.createElement('button');
+                VDbtn.addEventListener('click',()=>{
+                    togglePopup();
+                })
+                VDbtn.innerText = "View details";
+
+                div.appendChild(img);
+                div.appendChild(p)
+                div.appendChild(VDbtn);
+
+                let popup = `
+                    <div class="popup" id="popup-1">
+                    <div class="overlay"></div>
+                    <div class="content">
+                        <div class="close-btn" onclick="togglePopup()">&times;</div>
+                            <h1>Description</h1>    
+                            <p> Product Details here!!! </p>
+                        </div>
+                    </div>
+                `
+
+                li.innerHTML = popup;
+                li.appendChild(div);
                 ulList.appendChild(li);
+                let br = document.createElement('br');
+                ulList.appendChild(br);
+
                 if(i == products.length-1) noMorePrdct = true;
             }
             curr = limit;
@@ -41,3 +78,29 @@ rqst.addEventListener('load',()=>{
         
     })
 })
+
+let form = document.querySelector('form');
+form.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    if(document.getElementById('productImg').value ==""){
+        alert("Please select an image!");
+    }
+    else{
+        const data = new FormData(event.target);
+        const reqst = new XMLHttpRequest();
+        reqst.open('POST','/uploadProduct');
+        reqst.send(data);
+        reqst.addEventListener('load',()=>{
+            // data contains the img filename
+            const data = JSON.parse(reqst.responseText);
+            // obj.file = data.filename;
+            console.log(data);
+            console.log(data.filename);
+            form.reset();
+        });
+    }
+})
+
+function togglePopup(){
+    document.getElementById('popup-1').classList.toggle("active");
+}
